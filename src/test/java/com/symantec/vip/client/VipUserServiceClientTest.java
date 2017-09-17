407604199dafaecba9f862b520d607dfac97bd5a
Shantanu Gattani <shantanu_gattani@symantec.com>
2017-09-17 12:16:03 -0700

    Updated test to disable DNS caching

--- src/test/java/com/symantec/vip/client/VipUserServiceClientTest.java
+++ src/test/java/com/symantec/vip/client/VipUserServiceClientTest.java
@@ -2,12 +2,15 @@ package com.symantec.vip.client;
 
 import org.junit.Test;
 import com.symantec.vip.client.VipUserServiceClient;
+import java.security.Security;
 
 public class VipUserServiceClientTest {
 
     @Test
     public void myGetServerTimeTest(){
         System.setProperty("https.protocols", "TLSv1.1,TLSv1.2");
+        Security.setProperty("networkaddress.cache.ttl", "0");
+        Security.setProperty("networkaddress.cache.negative.ttl", "0");
 
         VipUserServiceClient client = new VipUserServiceClient();
 
